1a7c8a770ee5d1ee7c73c43071e720a2816e9c32
Jake Robin van der Ploeg <jake.r.g@gmail.com>
2018-11-03 15:49:51 +0100

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -9,6 +11,7 @@ let app = express();
 
 app.use(bodyParser.json());
 
+// POST single todo
 app.post('/todos', (req, res) => {
     // console.log(req.body);
     let todo = new Todo({
@@ -22,11 +25,32 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// GET all todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET single todo by id
+app.get('/todos/:id', (req, res) => {
+    let {id} = req.params;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        
+        res.send(todo);           
+    }, (e) => {
+        res.status(400).send();
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
