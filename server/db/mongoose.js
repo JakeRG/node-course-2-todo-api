4cb83a2364b7781b4a124ada3807382e9580e5e1
Jake Robin van der Ploeg <jake.r.g@gmail.com>
2018-11-03 18:07:16 +0100

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://heroku_51jjzz6h:4ifjhduf0c1rjg3ur1ct8457dv@ds151383.mlab.com:51383/heroku_51jjzz6h', { useNewUrlParser: true});
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://heroku_51jjzz6h:4ifjhduf0c1rjg3ur1ct8457dv@ds151383.mlab.com:51383/heroku_51jjzz6h', { useNewUrlParser: true});
+// 'mongodb://heroku_51jjzz6h:4ifjhduf0c1rjg3ur1ct8457dv@ds151383.mlab.com:51383/heroku_51jjzz6h'
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
